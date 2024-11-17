26d2336c69dd585a80f036543e31ce181d4ecca9
Róbert Kubík <pastierovic@gmail.com>
2024-11-17 09:01:27 +0100

    Changed version to 3.0 to align wiith GTK version it is using

--- src/constants.py
+++ src/constants.py
@@ -26,7 +26,7 @@ try:
 except Exception:
   import portability
 
-VERSION = '2.0'
+VERSION = '3.0'
 HOME_DIR = portability.get_home_directory()
 CONFIG_DIR = portability.get_config_directory()
 DATA_DIR = portability.get_data_directory()
